141fa0ff30258396db517c9c9db385eb443531cf
frabbit <frabbit77@googlemail.com>
2016-08-09 18:56:33 +0200

    only run i18n tests for now

--- tests/unit/src/unit/TestSpecification.hx
+++ tests/unit/src/unit/TestSpecification.hx
@@ -149,7 +149,7 @@ class RttiClass3 extends RttiClass1 {
 }
 
 #if !macro
-@:build(unit.UnitBuilder.build("src/unitstd"))
+@:build(unit.UnitBuilder.build("src/unitstd/haxe/i18n"))
 #end
 class TestSpecification extends Test {
 
